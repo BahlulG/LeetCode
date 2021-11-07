3a4fde0e957e46733b71d94395feecb1c8af7721
BahlulG <55544336+BahlulG@users.noreply.github.com>
2021-11-07 20:20:00 +0100

    adding findOriginalArray.py file

---
+++ findOriginalArray.py
@@ -0,0 +1,28 @@
+import collections
+
+
+def findOriginalArray(changed):
+    if len(changed) % 2 == 1:  # Return empty array, if array length is not even
+        return
+
+    original = []
+    numbers = collections.Counter(changed)
+
+    for num in sorted(changed):
+        double = num * 2
+
+        # In order to understand below rest of code, use https://pythontutor.com/visualize.html#mode=edit link
+        # copy paste all the code and view implementation step-by-step
+        if numbers[num] > 0 and numbers[double] > 0:
+            original.append(num)
+            numbers[num] -= 1
+            numbers[double] -= 1
+        elif num % 2 != 0 or num // 2 not in numbers:
+            return
+
+    return original
+
+
+array = [1, 3, 4, 2, 6, 8]
+
+print(findOriginalArray(array))
