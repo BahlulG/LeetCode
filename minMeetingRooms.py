3af74b513d1012c8b6d08bd9a33c8afd849c8f65
BahlulG <55544336+BahlulG@users.noreply.github.com>
2021-11-17 14:07:16 +0100

    Update minMeetingRooms.py

--- minMeetingRooms.py
+++ minMeetingRooms.py
@@ -1,5 +1,6 @@
 def minMeetingRooms(intervals):
-    if not intervals:  # if length of "intervals" is 0, return nothing
+    if not intervals:
+        # if length of "intervals" is 0 (meaning if "intervals" list is empty), return nothing
         return
 
     rooms = 0
@@ -12,15 +13,19 @@ def minMeetingRooms(intervals):
         starts.append(intervals[interval][0])
         ends.append(intervals[interval][1])
 
-    starts, ends = sorted(starts), sorted(ends)  # Sort both "starts" and "ends" lists
+    starts, ends = sorted(starts), sorted(ends)
+    # Sort both "starts" and "ends" lists
 
     for start in starts:
-        while ends[curr] <= start:  # In sorted "ends", if end is smaller or equal to start, it means we have one available room for another meeting. Because there is not time clash
+        while ends[curr] <= start:
+            # In sorted "ends", if end is smaller or equal to start, it means we have one available room for another meeting. Because there's no time clash
             available += 1
             curr += 1
-        if available:  # here we check, if end is greater than start, then we will use on of the available rooms
+        if available:
+            # here we check, if end is greater than start, then we will use on of the available rooms
             available -= 1
-        else:  # here we are checking if end is greater than start and no available room, we need one more room
+        else:
+            # here check if end is greater than start and no available room, we need one more room for another meeting
             rooms += 1
     return rooms
 
