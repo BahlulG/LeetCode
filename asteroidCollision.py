1a2f3c00cea64c7acb878a1fe8714ca38f827220
BahlulG <55544336+BahlulG@users.noreply.github.com>
2021-11-21 15:39:37 +0100

    Update asteroidCollision.py

--- asteroidCollision.py
+++ asteroidCollision.py
@@ -3,21 +3,21 @@ def asteroidCollision(asteroids):
 
     for asteroid in asteroids:
         while stack and stack[-1] > 0 > asteroid:
-            # If 'asteroid' smaller than last element of the 'stack' then break
+            # If the 'asteroid' smaller than the last element of the 'stack', then break
             if stack[-1] + asteroid > 0:
                 break
 
-            # If 'asteroid' bigger than last element of the 'stack' then delete last element of the 'stack'
-            # until the end of the 'stack', or until 'asteroid' smaller than last element of the 'stack'
+            # If the 'asteroid' bigger than the last element of the 'stack', then delete the last element of the 'stack'
+            # until the end of the 'stack', or until the 'asteroid' smaller than the last element of the 'stack'
             elif stack[-1] + asteroid < 0:
                 stack.pop(-1)
 
-            # If 'asteroid' is equal to the last element of the 'stack' then delete last element of the 'stack'
+            # If the 'asteroid' is equal to the last element of the 'stack', then delete the last element of the 'stack'
             else:
                 stack.pop(-1)
                 break
 
-        # If none of above conditions meet, then append the element to the 'stack'
+        # If none of the above conditions meet, then append the 'asteroid' to the 'stack'
         else:
             stack.append(asteroid)
 
