6e08405286b632547ba6decf3f6bd456fad7317e
BahlulG <55544336+BahlulG@users.noreply.github.com>
2021-11-29 17:30:12 +0100

    Update Swap Nodes in Pairs.py

--- Swap Nodes in Pairs.py
+++ Swap Nodes in Pairs.py
@@ -7,23 +7,23 @@ class Solution:
     def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if not head:  # If Linked-List is empty, return empty head
             return head
-        elif not head.next:  # If Linked-List consist of one element, return empty head
+        elif not head.next:  # If Linked-List consist of one element, return head
             return head
         else:
-            # Keep track of first and second elements, in order to do in-place value change
+            # Keep track of the first and second elements, in order to do in-place value change
             previous = head
             current = head.next
 
             while current:
-                a = previous.val  # Keep track of values for exchange
-                b = current.val  # Keep track of values for exchange
-                previous.val = b  # Exchange the values of 'previous' and 'current'
-                current.val = a  # Exchange the values of 'previous' and 'current'
+                a = previous.val  # Keep track of values for swap
+                b = current.val  # Keep track of values for swap
+                previous.val = b  # Swap the values of 'previous' and 'current'
+                current.val = a  # Swap the values of 'previous' and 'current'
 
                 if previous.next.next and current.next.next:
-                    # If there still Nodes to change, then continue
+                    # If there still Nodes to swap the values, then move two Nodes futher
                     previous = previous.next.next
                     current = current.next.next
                 else:
-                    # If there are no more Nodes, return the Linked-List
+                    # If there are no more Nodes to swap the values, return the Linked-List
                     return head
