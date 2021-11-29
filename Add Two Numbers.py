d85b2aefe8310fdf94338772c68be1ad10d7dbac
BahlulG <55544336+BahlulG@users.noreply.github.com>
2021-11-29 10:22:01 +0100

    Update Add Two Numbers.py

--- Add Two Numbers.py
+++ Add Two Numbers.py
@@ -11,16 +11,17 @@ class Solution:
             return l1
         else:
             carry = 0
-            newHead = Tracker = ListNode
+            newHead = tracker = ListNode
             # Keep track of the pointer: 'newHead' will change the pointer to the next Node,
             # each time we will add new Node
-            # But, we need 'Tracker', in order to return the linked-list, from the beginning,
+            # But, we need the 'Tracker', in order to return the linked-list, from the beginning,
             # at the end of the while loop
 
             while l1 or l2 or carry:
                 # If any of 'l1 or l2 or carry' is true, continue running while loop
                 # There can be the situation that 'l1' length is bigger than 'l2', or vice-versa, or we gone through
                 # the lists, but we still have some value in 'carry', which we have to add to 'newHead'
+                
                 if l1:
                     carry += l1.val
                     l1 = l1.next
@@ -33,4 +34,4 @@ class Solution:
                 carry = carry // 10  # Keep floor division to next Node: 15 // 10 = 1
                 newHead = newHead.next  # Move pointer to next 'newHead'
 
-            return Tracker.next  # Return new Linked-list from the beginning
+            return tracker.next  # Return new Linked-list from the beginning
