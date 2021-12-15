3dd746043a00c3fff562337308b10859a10e4834
BahlulG <55544336+BahlulG@users.noreply.github.com>
2021-12-15 09:08:35 +0100

    Update Maximum Depth of Binary Tree.py

--- Maximum Depth of Binary Tree.py
+++ Maximum Depth of Binary Tree.py
@@ -4,6 +4,7 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         if root is None:
@@ -12,4 +13,9 @@ class Solution:
         leftDepth = self.maxDepth(root.left)
         rightDepth = self.maxDepth(root.right)
 
+        
+        # Recursive call left and right subtree, will go until the Node is None
+        # Then, the last recursion's return value will be equal to 1. In last call, either left or right Node will exist.
+        # Everytime recursion 'return' will add +1 to the maximum value. In our case, maximum value out of left or right will be one of them.
+        # Every recursion 'return' will add +1 to the maximum value.
         return max(leftDepth, rightDepth) + 1
